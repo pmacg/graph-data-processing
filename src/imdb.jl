fd8fcff8460cdc105edcc2b25f69f7c0f9bd1031
Peter Macgregor <macgregor.pr@gmail.com>
2021-04-27 21:59:52 +0100

    Save IMDB cluster data

--- src/imdb.jl
+++ src/imdb.jl
@@ -16,6 +16,9 @@ struct Imdb
     # actor ID -> actor name
     actors::Dict{String, String}
 
+    # actor ID -> cluster ID
+    clusters::Dict{String, Int64}
+
     # film ID -> array of credited people's IDs
     credits::Dict{String, Array{String}}
 end
@@ -55,6 +58,7 @@ function loadimdb(directory)
     # Grouping by movie, construct the credits dictionary.
     groups = groupby(df, :tconst)
     credits = Dict()
+    clusters = Dict()
     for group in groups
         this_tconst = group[1, :tconst]
         this_actors = group[group.category .== "actor", :].nconst
@@ -62,14 +66,26 @@ function loadimdb(directory)
         this_directors = group[group.category .== "director", :].nconst
         this_principals = []
 
-        # Keep at most the first two of each category
-        append!(this_principals, this_actresses[1 : min(2, length(this_actresses))])
-        append!(this_principals, this_actors[1 : min(2, length(this_actors))])
-        append!(this_principals, this_directors[1 : min(2, length(this_directors))])
+        # Update the clusters dictionary
+        for actor in this_actors
+            clusters[actor] = 1
+        end
+        for actress in this_actresses
+            clusters[actress] = 2
+        end
+        for director in this_directors
+            clusters[director] = 3
+        end
+
+        # Keep at most the first k of each category
+        k = 1
+        append!(this_principals, this_actresses[1 : min(k, length(this_actresses))])
+        append!(this_principals, this_actors[1 : min(k, length(this_actors))])
+        append!(this_principals, this_directors[1 : min(k, length(this_directors))])
         credits[this_tconst] = this_principals
     end
 
-    return Imdb(films, actors, credits)
+    return Imdb(films, actors, clusters, credits)
 end
 
 """
@@ -78,10 +94,12 @@ end
 
 Store the IMDB credit hyperaph in the edgelist format.
 
-Create three files:
+Create five files:
   - filename.edgelist
   - filename.edges
   - filename.vertices
+  - filename.gt
+  - filename.clusters
 
 If only the imdb directory is specified, then the data is loaded, and then saved, with
 filename="credit".
@@ -98,6 +116,8 @@ function writeCreditEdgelist(imdbData::Imdb, filename::String)
     edgelistFile = open("$filename.edgelist", "w")
     edgesFile = open("$filename.edges", "w")
     verticesFile = open("$filename.vertices", "w")
+    gtFile = open("$filename.gt", "w")
+    clusterFile = open("$filename.clusters", "w")
 
     # We will construct a dictionary of person IDs -> node IDs as we go along.
     person_node = Dict()
@@ -114,10 +134,18 @@ function writeCreditEdgelist(imdbData::Imdb, filename::String)
                 person_node[person_id] = vᵢ
                 vᵢ += 1
 
+                # Write the name of the vertex
                 if person_id ∈ keys(imdbData.actors)
                     println(verticesFile, imdbData.actors[person_id])
                 else
                     println(verticesFile, "missing")
+                end
+
+                # Write the cluster of the vertex
+                if person_id ∈ keys(imdbData.clusters)
+                    println(gtFile, imdbData.clusters[person_id])
+                else
+                    println(verticesFile, "-1")
                 end
             end
 
@@ -128,9 +156,14 @@ function writeCreditEdgelist(imdbData::Imdb, filename::String)
         print(edgelistFile, "\n")
     end
 
+    # Write the cluster ID file
+    print(clusterFile, "actor\nactress\ndirector")
+
     close(edgelistFile)
     close(edgesFile)
     close(verticesFile)
+    close(gtFile)
+    close(clusterFile)
 end
 
 function writeCreditEdgelist(imdbDirectory::String)
